5605b8d82dc2e8d8ddac39c91668f2586670f304
MARTIN <Martonyx>
2022-09-29 02:53:26 +0100

    updated

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x875e8794DE1D16B833c00162A81ef59Ce6aF03c3"
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/"
+const METADATA_URL = "https://martonyx-nft-collection.vercel.app/api/"
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
